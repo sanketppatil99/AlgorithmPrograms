dd9a01aeb23f3ee62a260a9a9b8ff8cc43d01628
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-04 00:18:09 +0530

    An Anagram Detection

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@
         {
             Console.WriteLine("Welcome to Algorithm Programs");
 
-            MergeSort merge = new MergeSort();
-            merge.mergeSort();
+            Anagram ana = new Anagram();
+            ana.anagram();
         }
     }
 }
