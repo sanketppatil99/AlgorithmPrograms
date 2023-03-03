4378845e6b6ef039abbd07bd638e8bc55cf0696f
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-04 00:19:42 +0530

    An Anagram Detection

---
+++ Anagram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class Anagram
+    {
+        public void anagram()
+        {
+            string str1 = "heart";
+            string str2 = "earth";
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string s1 = new string(ch1);
+            string s2 = new string(ch2);
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Anagram");
+
+            }
+            else
+            {
+                Console.WriteLine("Not Anagram");
+            }
+        }
+    }
+}
