449fa8f938608c4a8453530c46a4ce608b9d0506
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-04 00:34:19 +0530

    PalindromCheck

---
+++ PalindromChecker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class PalindromChecker
+    {
+        public void palindrom(int num1, int num2)
+        {
+            int c, rev, temp, i, num;
+            Console.WriteLine("The Prime Palindrome Numbers are :");
+            for (num = num1; num <= num2; num++)
+            {
+                temp = num;
+                c = 0;
+                rev = 0;
+                for (i = 1; i <= temp; i++)
+                {
+                    if (temp % i == 0)
+                        c++;
+                }
+                if (c == 2)
+                {
+                    while (temp > 0)
+                    {
+                        rev = rev * 10 + (temp % 10);
+                        temp = temp / 10;
+                    }
+                    if (rev == num)
+                    {
+                        Console.WriteLine(num);
+                    }
+                }
+            }
+        }
+    }
+}
