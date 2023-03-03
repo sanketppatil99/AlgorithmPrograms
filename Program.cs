13a0909cfea9900a739080b56317a06a0d68a082
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-03 23:57:10 +0530

    InsertionSort

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Algorithm Programs");
+
+            InsertionSort insertion = new InsertionSort();
+            insertion.insertionSort();
         }
     }
 }
